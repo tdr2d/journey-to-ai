6c16c9f637540ea3a8d4756ecfa2a53b336ee10e
tdr2d <28861767+tdr2d@users.noreply.github.com>
2024-01-12 22:10:55 +0100

    added find_digit_contour function

--- modules/01_ocr_digits/main.py
+++ modules/01_ocr_digits/main.py
@@ -4,12 +4,55 @@ import numpy as np
 import tqdm
 import cv2
 from utils import *
+from rectangle import Rectangle
 import math
 ssl._create_default_https_context = ssl._create_unverified_context
 
-THRESHOLD = 127
+THRESHOLD = 127 # optimizing threshold https://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/OWENS/LECT2/node3.html
 SAMPLE = 100
+PIXELS_X = 28
+PIXELS_Y = 28
+MIN_CONTOUR_THRESHOLD = 0.05
 
+
+def find_digit_contour(gray_img):
+    # RETR_EXTERNAL child contour are ignored
+    # CHAIN_APPROX_NONE return closest geometry fit
+    contours = cv2.findContours(gray_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]  
+    min_contour_height = int(img.shape[0] * MIN_CONTOUR_THRESHOLD)
+    min_contour_width = int(img.shape[1] * MIN_CONTOUR_THRESHOLD)
+    contour_quandidates = [] 
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        if w > min_contour_width and w < img.shape[1] - 1 and h > min_contour_height and h < img.shape[0] - 1:
+            contour_quandidates.append(Rectangle(x,y,w,h))
+
+    if len(contour_quandidates):
+        big_contour = contour_quandidates.pop()
+        for r in contour_quandidates:
+            big_contour = big_contour.merge(r)
+        return big_contour
+    return None
+
+
+def center_in_background(background_img, img):
+    assert background_img.shape[0] > img.shape[0]
+    assert background_img.shape[1] > img.shape[1]
+
+    ## (1) Find centers
+    pt1 = int(background_img.shape[1] / 2), int(background_img.shape[0] / 2)
+    pt2 = int(img.shape[1] / 2), int(img.shape[0] / 2)
+
+    ## (2) Calc offset
+    dx = pt1[0] - pt2[0]
+    dy = pt1[1] - pt2[1]
+
+    ## (3) do slice-op `paste`
+    h,w = img.shape[:2]
+
+    dst = background_img.copy()
+    dst[dy:dy+h, dx:dx+w] = img
+    return dst
 
 def preprocessing(img):
     if img.ndim == 3:
@@ -21,23 +64,23 @@ def preprocessing(img):
     out = skeletonize(out)
     out = fill_shapes(out)
     out = skeletonize(out)
+    # out = cv2.bitwise_not(out)
 
-    out = cv2.bitwise_not(out)
     out_color = cv2.cvtColor(out, cv2.COLOR_GRAY2BGR)
-    contours, hierarchy = cv2.findContours(out, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(len(contours))
-    for c in contours:
-        cv2.drawContours(out_color, contours, -1, (0,0,255), 1)
+    digit_contour = find_digit_contour(out)
+    if digit_contour is not None:
+        digit_contour.debug(out_color)
+        
+        # sub_image = digit_contour.get_sub_image(out)
+        # mask_width = max(digit_contour.w, digit_contour.h) + 3
+        # mask = np.zeros((mask_width, mask_width), np.uint8)
 
-        # for eps in np.linspace(0.001, 0.05, 10):
-        #     peri = cv2.arcLength(c, True)
-        #     approx = cv2.approxPolyDP(c, eps * peri, True)
-        #     if len(approx == 4):
-        #         cv2.drawContours(out_color, [approx], -1, (0,0,255), 1)
-        #         break
+        # out_color = cv2.resize(sub_image, (PIXELS_X, PIXELS_Y))
+        # mask = center_in_background(mask, sub_image)
+
+        # todo rotate
+        # mask = cv2.resize(mask, (PIXELS_X, PIXELS_Y))
     
-    # img = cv2.resize(img, (28, 28))
-
     return out_color
 
 def combine(ref, out):
